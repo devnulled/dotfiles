39587d33d05255a39d7207caee6a0d454669ac78
Brandon Harper <brandon.harper@twcable.com>
2013-11-13 14:55:20 -0700

    Trying to get my shell config working

--- home/.oh-my-fish.fish
+++ home/.oh-my-fish.fish
@@ -6,8 +6,8 @@ set PATH /usr/local/opt/curl/bin /usr/local/apache-maven-3.0.3 ~/bin ~/.bin $PAT
 set PATH ~/.scripts /usr/bin:/bin /usr/sbin /sbin /usr/local/bin /usr/X11/bin $PATH
 set PATH /Applications/git-annex.app/Contents/MacOS $PATH
 
-set -U JAVA7_HOME $(/usr/libexec/java_home -v 1.7.0)
-set -U JAVA6_HOME $(/usr/libexec/java_home -v 1.6.0)
+set -U JAVA7_HOME (/usr/libexec/java_home -v 1.7.0)
+set -U JAVA6_HOME (/usr/libexec/java_home -v 1.6.0)
 
 set -g JAVA_HOME $JAVA6_HOME
 
