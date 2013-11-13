0fe8dfc0289acea30a922a6426dea06ef2281850
Brandon Harper <brandon.harper@twcable.com>
2013-11-13 14:48:17 -0700

    Doh, I thought I had already deleted that file

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -14,28 +14,5 @@ set fish_theme robbyrussell
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-
-################ START CUSTOMIZATIONS ################
-
-# Env variables
-
-set PATH /usr/local/opt/curl/bin /usr/local/apache-maven-3.0.3 ~/bin ~/.bin $PATH 
-set PATH ~/.scripts /usr/bin:/bin /usr/sbin /sbin /usr/local/bin /usr/X11/bin $PATH
-set PATH /Applications/git-annex.app/Contents/MacOS $PATH
-
-set -U JAVA7_HOME $(/usr/libexec/java_home -v 1.7.0)
-set -U JAVA6_HOME $(/usr/libexec/java_home -v 1.6.0)
-
-set -g JAVA_HOME $JAVA6_HOME
-
-set -U TOMCAT_HOME '/usr/local/apache-tomcat'
-
-set -U CATALINA_OPTS '-XX:MaxPermSize=1024M -Xmx2680M -Xms1024M -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote'
-
-set -U GRADLE_HOME '/usr/local/bin/gradle'
-
-set -U GRADLE_OPTS '-XX:MaxPermSize=1024M -Xmx2680M -Xms1024M -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote'
-
-set -U PYTHONPATH=/usr/local/lib/python/site-packages/
-
-################ END CUSTOMIZATIONS ################
+# Load my own overrides from my own local oh-my-fish config
+. .oh-my-fish.fish
